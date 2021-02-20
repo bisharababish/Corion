824be5b4265a176fdb950568511ca2059e156ec3
bishara.babish23@gmail.com <bishara.babish23@gmail.com>
2021-02-20 13:06:22 +0200

    first commit

--- fun/gay.js
+++ fun/gay.js
@@ -1,6 +1,13 @@
-
+const Discord = require('discord.js')
 exports.run = async (bot,message,args) => {
-   
+   let member = message.mentions.user.first() || message.author
+   let rng = Math.floor(math.random() * 101);
+   const howgay =  new Discord.MessageEmbed()
+   .setTitle('Gay Machine Calculator')
+   .setDescription(`${member.username} is + rng + "% Gay🏳️‍🌈 `)
+   .setColor("RANDOM")
+   message.channel.send(howgay)
+
 }
 
         exports.help = {
